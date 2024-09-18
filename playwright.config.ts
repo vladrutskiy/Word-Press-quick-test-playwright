5617169c1aeaf06f3c680de4e0f37d5f52258f72
Vlad Rutskiy <vladrytskiy@gmail.com>
2024-09-17 21:58:17 -0600

    no major changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
         
-    baseURL: 'https://rutskyi.yotako.com',
+    baseURL: 'https://rutskyi.yotako.com/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
